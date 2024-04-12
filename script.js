03c1ac1e22dde09560993d208e87bcda903a64e5
NoctSword <xxxdarkcentxxx@gmail.com>
2024-04-12 09:14:26 -0600

    a

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ class Semaforo {
     }
   
     avanzar(otrosAutos) {
-      if (otrosAutos.some(auto => auto.x > this.x && auto.x - this.x < Math.floor(Math.random() * 51) + 75)) {
+      if (otrosAutos.some(auto => auto.x > this.x && auto.x - this.x < Math.floor(Math.random() * 51) + 76)) {
         // Si hay autos delante, detenerse
         this.estado = 'detenido';
       } else {
